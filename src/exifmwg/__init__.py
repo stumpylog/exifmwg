3c287dab3bbc47019f953297eb68233f29128c54
Trenton Holmes <797416+stumpylog@users.noreply.github.com>
2025-06-07 13:13:54 -0700

    Exports additional classes

--- src/exifmwg/__init__.py
+++ src/exifmwg/__init__.py
@@ -1,5 +1,23 @@
+from exifmwg._core import DimensionsStruct
+from exifmwg._core import ImageMetadata
+from exifmwg._core import KeywordInfoModel
+from exifmwg._core import KeywordStruct
+from exifmwg._core import RegionInfoStruct
+from exifmwg._core import RegionStruct
+from exifmwg._core import XmpAreaStruct
 from exifmwg._core import clear_existing_metadata
 from exifmwg._core import read_metadata
 from exifmwg._core import write_metadata
 
-__all__ = ["clear_existing_metadata", "read_metadata", "write_metadata"]
+__all__ = [
+    "DimensionsStruct",
+    "ImageMetadata",
+    "KeywordInfoModel",
+    "KeywordStruct",
+    "RegionInfoStruct",
+    "RegionStruct",
+    "XmpAreaStruct",
+    "clear_existing_metadata",
+    "read_metadata",
+    "write_metadata",
+]
